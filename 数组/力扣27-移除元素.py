cc5d0d5b00914bedfb85976e75122fa60ffafca8
mengjiehe <1579457263@qq.com>
2024-05-27 16:43:57 +0800

    力扣-移除元素-快慢指针法

---
+++ 数组/力扣27-移除元素.py
@@ -0,0 +1,19 @@
+# 双指针（快慢指针）删除元素
+nums = [0, 1, 2, 2, 3 , 0, 4, 2]
+val = 2
+
+nums_length = len(nums)
+fast = 0
+low = 0
+while fast < len(nums):
+    nums[low] = nums[fast]
+    # 当low指针值等于val时，需要停在原地根据fast指针赋值；否则移动
+    if nums[low] != val:
+        low += 1
+    else:
+        nums_length -= 1
+    # fast指针每次正常移动
+    fast += 1
+
+print(nums, nums_length)
+

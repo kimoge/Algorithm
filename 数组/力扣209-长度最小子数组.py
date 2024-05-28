8b6117b3fe8d92afab5bc02c6e234f0a63f2d364
mengjiehe <1579457263@qq.com>
2024-05-29 06:41:57 +0800

    力扣-长度最小子数组-格式修改

--- 数组/力扣209-长度最小子数组.py
+++ 数组/力扣209-长度最小子数组.py
@@ -1,20 +1,19 @@
 # 双指针
 target = 7
-nums = [2,3,1,2,4,3]
+nums = [2, 3, 1, 2, 4, 3]
 
 # 左闭右闭区间
-l = 0
-r = 0
+left = 0
+right = 0
 res = 0
 min_length = len(nums)
-sum_nums = 0
-while l <= r < len(nums):
-    res += nums[r]
-    sum_nums += nums[r]
+# while left <= right < len(nums):
+while right < len(nums):
+    res += nums[right]
     while res >= target:
-        min_length = min(r - l + 1, min_length)
-        res -= nums[l]
-        l += 1
-    r += 1
-print(sum_nums)
+        min_length = min(right - left + 1, min_length)
+        res -= nums[left]
+        left += 1
+    right += 1
+
 print(min_length)

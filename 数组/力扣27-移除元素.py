df8cc830a234c783686e028fa7e2b6c3d2493595
mengjiehe <1579457263@qq.com>
2024-06-02 09:22:44 +0800

    力扣-移除元素-改进

--- 数组/力扣27-移除元素.py
+++ 数组/力扣27-移除元素.py
@@ -2,7 +2,6 @@
 nums = [0, 1, 2, 2, 3 , 0, 4, 2]
 val = 2
 
-nums_length = len(nums)
 fast = 0
 low = 0
 while fast < len(nums):
@@ -10,10 +9,9 @@ while fast < len(nums):
     # 当low指针值等于val时，需要停在原地根据fast指针赋值；否则移动
     if nums[low] != val:
         low += 1
-    else:
-        nums_length -= 1
+
     # fast指针每次正常移动
     fast += 1
 
-print(nums, nums_length)
+print(nums, low)
 

60b3d51491e6cba58f3530fc11c0487155062bee
mengjiehe <1579457263@qq.com>
2024-06-02 09:32:41 +0800

    力扣-反转字符串中的单词-改进

--- 字符串/力扣151-反转字符串中的单词.py
+++ 字符串/力扣151-反转字符串中的单词.py
@@ -13,7 +13,6 @@ s_l = list(s)
 # 双指针消除空格
 low = 0
 fast = 0
-length = len(s_l)
 while fast < len(s_l):
     # 找到要填充目标
     if s_l[fast] != " ":
@@ -21,30 +20,27 @@ while fast < len(s_l):
         if low != 0:
             s_l[low] = " "
             low += 1
-            length += 1
+            
         # 持续赋值一个单词
         while fast < len(s_l) and s_l[fast] != " ":
             s_l[low] = s_l[fast]
             low += 1
             fast += 1
 
-    else:
-        # 遇见空格，长度-1
-        length -= 1
-        fast += 1
-# print(s_l[:length])
+    fast += 1
+print(s_l[:low])
 
-myReverse(s_l, 0, length-1)
+myReverse(s_l, 0, low-1)
 
 l = 0
 r = 0
 # 左闭右开字符串
-while r <= length:
-    if r == length or s_l[r] == " ":
+while r <= low:
+    if r == low or s_l[r] == " ":
         myReverse(s_l, l, r-1)
         r += 1
         l = r
     r += 1
 
-print("".join(s_l[:length]))
+print("".join(s_l[:low]))
 

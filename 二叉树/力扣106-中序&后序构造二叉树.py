9cff7002fd76308bcae67ca9f5c00153dc197341
mengjiehe <1579457263@qq.com>
2024-06-08 15:08:21 +0800

    力扣-中序&后序构造二叉树

---
+++ 二叉树/力扣106-中序&后序构造二叉树.py
@@ -0,0 +1,22 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def buildTree(self, inorder: List[int], postorder: List[int]) -> Optional[TreeNode]:
+        pass
+
+    def recur(self, inorder, postorder):
+        if not postorder or not inorder:
+            return None
+
+        root_val = postorder.pop()
+        root = TreeNode(root_val)
+        root_index = inorder.index(root_val)
+
+        root.right = self.recur(inorder[root_index+1:], postorder)
+        root.left = self.recur(inorder[:root_index], postorder)
+
+        return root

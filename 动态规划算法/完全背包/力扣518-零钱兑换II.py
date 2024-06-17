9cd449bb7a02b19f53cd75e9361d126c22efab15
mengjiehe <1579457263@qq.com>
2024-06-17 15:44:22 +0800

    零钱兑换II

---
+++ 动态规划算法/完全背包/力扣518-零钱兑换II.py
@@ -0,0 +1,22 @@
+# 完全背包
+
+# 1、dp[j]:j容量背包有dp[j]种装法
+# 2、递推公式：dp[j] += dp[j - coins[i]]
+# 3、遍历顺序：顺序，由于是组合方案，所以先物品后背包
+# 4、初始化：dp[0] = 1,其余为0
+# 5、打印dp数组
+
+
+amount = 5
+coins = [1, 2, 5]
+
+
+dp = [0] * (amount+1)
+dp[0] = 1
+
+for i in range(len(coins)):
+    for j in range(coins[i], len(dp)):
+        dp[j] += dp[j-coins[i]]
+
+# print(dp)
+print(dp[-1])

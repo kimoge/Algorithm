a7ba4c904fd815032bcd602114f6feb2edeb361b
mengjiehe <1579457263@qq.com>
2024-06-19 09:13:00 +0800

    最长递增子序列

--- 动态规划算法/序列、子串/力扣300-最长递增子序列.py
+++ 动态规划算法/序列、子串/力扣300-最长递增子序列.py
@@ -1,5 +1,5 @@
 # 1、dp[i]:确定将i加入子序列时严格递增子序列最长长度
-# 2、if nums[i] > nums[i-1]: dp[i] = max(dp[i], dp[j] + 1)
+# 2、if nums[i] > nums[j]: dp[i] = max(dp[i], dp[j] + 1) j:0~i-1
 # else dp[i-1]
 # 3、dp[0] = 1
 # 4、从左到右

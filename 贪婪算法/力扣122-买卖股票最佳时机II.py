c8b8f22a04a4b511ef0efced2f3468d307d84776
mengjiehe <1579457263@qq.com>
2024-06-22 10:53:07 +0800

    买卖股票最佳时机II

--- 贪婪算法/力扣122-买卖股票最佳时机II.py
+++ 贪婪算法/力扣122-买卖股票最佳时机II.py
@@ -1,12 +1,8 @@
 prices = [7,1,5,3,6,4]
 
-low = max(prices)
 res = 0
 
-for i in prices:
-    # 截止目前为止最小值
-    low = min(low, i)
-    # 截止目前为止最大间距
-    res = max(res, i - low)
+for i in range(1, len(prices)):
+    res += max(0, prices[i] - prices[i-1])
 
 print(res)

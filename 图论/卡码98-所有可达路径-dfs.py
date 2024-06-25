5c920d6415ba9f99e62b731faa0a56df4bc9f0ef
mengjiehe <1579457263@qq.com>
2024-06-25 09:20:29 +0800

    所有可达路径dfs版

--- 图论/卡码98-所有可达路径-dfs.py
+++ 图论/卡码98-所有可达路径-dfs.py
@@ -1,4 +1,7 @@
 def dfs(graph, x, res, path):
+    """
+    邻接矩阵的dfs方法
+    """
     if x == n:
         res.append(path.copy())
         return
@@ -10,18 +13,50 @@ def dfs(graph, x, res, path):
             path.pop()
 
 
+def dfs_(graph, x, res, path):
+    """
+    邻接表的dfs
+    """
+    if x == n:
+        res.append(path.copy())
+        return
+
+    for i in graph[x]:
+        path.append(i)
+        dfs_(graph, i, res, path)
+        path.pop()
+
+
 n, m = map(int, input().split())
 
-# method1：使用邻接矩阵
-graph = [[0] * (n + 1) for _ in range(n + 1)]
+# # method1：使用邻接矩阵
+# graph = [[0] * (n + 1) for _ in range(n + 1)]
+#
+# for i in range(m):
+#     s, t = map(int, input().split())
+#     graph[s][t] = 1
+#
+# res = []
+# dfs(graph, 1, res, [1])
+# # print(res)
+# if not res:
+#     print(-1)
+# else:
+#     for i in res:
+#         print(" ".join(list(map(str, i))))
+
+
+# method2：使用邻接表
+graph = [[] for i in range(n + 1)]
 
 for i in range(m):
     s, t = map(int, input().split())
-    graph[s][t] = 1
-
+    graph[s].append(t)
 res = []
-dfs(graph, 1, res, [1])
+dfs_(graph, 1, res, [1])
 # print(res)
-
-for i in res:
-    print(" ".join(list(map(str, i))))
+if not res:
+    print(-1)
+else:
+    for i in res:
+        print(" ".join(list(map(str, i))))
